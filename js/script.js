94f41b4b8ba27a961603ba70c871668f0e06eb3a
Muhamed Abd-elrhman <mu.abdo1996@gmail.com>
2024-08-07 01:56:21 +0300

    last two commit

--- js/script.js
+++ js/script.js
@@ -3,4 +3,23 @@ $(function(){
     $('#includeheader').load('header.html');
 })
 
+$(function(){
+    $('#player').load('player-bar.html')
+})
 
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 100) {
+        $('.channel-podcast').addClass('fixed-web');
+    } else {
+        $('.channel-podcast').removeClass('fixed-web');
+    }
+})
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 100) {
+        $('.navbar').addClass('fix-header');
+    } else {
+        $('.navbar').removeClass('fix-header');
+    }
+})
