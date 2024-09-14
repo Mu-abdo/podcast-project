23f0a6f952fc33097179b762e26de58491a640a2
Muhamed Abd-elrhman <mu.abdo1996@gmail.com>
2024-09-14 19:33:28 +0300

    pod-edit

--- js/script.js
+++ js/script.js
@@ -18,3 +18,48 @@ $(window).scroll(function() {
         $('.navbar').removeClass('fix-header');
     }
 })
+
+
+
+
+var swiper = new Swiper('.mySwiper', {
+    slidesPerView: 1, // عرض شريحة واحدة افتراضيًا
+    spaceBetween: 20, // مسافة بين الشرائح
+    loop: true, // تكرار السلايدر
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      640: {  // لأجهزة الهواتف
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      768: {  // لأجهزة التابلت
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1024: {  // لأجهزة الكمبيوتر المحمولة والشاشات الكبيرة
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    },
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const navbarToggle = document.getElementById('navbarToggle');
+    const navbarCollapse = document.getElementById('navbarSupportedContent');
+  
+    navbarToggle.addEventListener('click', function() {
+      if (navbarCollapse.classList.contains('show')) {
+        navbarCollapse.classList.remove('show');
+      } else {
+        navbarCollapse.classList.add('show');
+      }
+    });
+  });
+  
