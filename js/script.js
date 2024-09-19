9203175d5973350fc518cc4d71fd0647ab38cb18
Muhamed Abd-elrhman <mu.abdo1996@gmail.com>
2024-09-19 13:27:13 +0300

    push responsive

--- js/script.js
+++ js/script.js
@@ -50,16 +50,5 @@ var swiper = new Swiper('.mySwiper', {
     },
   });
 
-  document.addEventListener('DOMContentLoaded', function() {
-    const navbarToggle = document.getElementById('navbarToggle');
-    const navbarCollapse = document.getElementById('navbarSupportedContent');
-  
-    navbarToggle.addEventListener('click', function() {
-      if (navbarCollapse.classList.contains('show')) {
-        navbarCollapse.classList.remove('show');
-      } else {
-        navbarCollapse.classList.add('show');
-      }
-    });
-  });
+
   
